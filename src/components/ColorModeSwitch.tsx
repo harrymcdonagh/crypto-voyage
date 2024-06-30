7f41ce98b0a64af9447387655b3e6fa54f8210d3
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-06-30 15:20:53 +0300

    Imporved Sidepanel

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,15 +1,19 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode, Icon } from "@chakra-ui/react";
+import { FaMoon } from "react-icons/fa";
+import { IoSunny } from "react-icons/io5";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const c = colorMode === "dark";
+  
   return (
     <HStack>
       <Switch
         colorScheme="green"
-        isChecked={colorMode === "dark"}
+        isChecked={c}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Icon as={c ? FaMoon : IoSunny} boxSize={4} />
     </HStack>
   );
 };
--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -6,7 +6,18 @@ import {
     DrawerOverlay,
     DrawerContent,
     DrawerCloseButton,
+    Stack,
+    Box,
+    VStack,
   } from '@chakra-ui/react';
+
+  import { MdAccountCircle } from "react-icons/md";
+  import { FaFireAlt } from "react-icons/fa";
+  import { IoPieChart } from "react-icons/io5";
+  import { FaStar } from "react-icons/fa6";
+  import { Button } from '@chakra-ui/react';
+  import ColorModeSwitch from './ColorModeSwitch';
+
   
   interface Props {
     isOpen: boolean;
@@ -16,20 +27,61 @@ import {
   const SidePanel = ({ isOpen, toggleSidePanel }: Props) => {
     return (
       <Drawer placement="right" isOpen={isOpen} onClose={toggleSidePanel}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Menu</DrawerHeader>
-            <DrawerBody>
-              <p>Item 1</p>
-              <p>Item 2</p>
-              <p>Item 3</p>
-            </DrawerBody>
-            <DrawerFooter>
-              <p>Footer</p>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth='1px'>
+            Menu
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack spacing="15px" justify="center" align="center" w="full">
+              <Box w="full" display="flex" justifyContent="center">
+                <ColorModeSwitch />
+              </Box>
+              <Box w="full" display="flex" justifyContent="center">
+                <Button
+                  leftIcon={<MdAccountCircle />}
+                  colorScheme="blue"
+                  variant="outline"
+                  w="60%"
+                >
+                  Login
+                </Button>
+              </Box>
+              <Box w="full" display="flex" justifyContent="center">
+                <Button
+                  leftIcon={<FaFireAlt />}
+                  colorScheme="blue"
+                  variant="outline"
+                  w="60%"
+                >
+                  Trending
+                </Button>
+              </Box>
+              <Box w="full" display="flex" justifyContent="center">
+                <Button
+                  leftIcon={<IoPieChart />}
+                  colorScheme="blue"
+                  variant="outline"
+                  w="60%"
+                >
+                  Portfolio
+                </Button>
+              </Box>
+              <Box w="full" display="flex" justifyContent="center">
+                <Button
+                  leftIcon={<FaStar />}
+                  colorScheme="blue"
+                  variant="outline"
+                  w="60%"
+                >
+                  Watchlist
+                </Button>
+              </Box>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
       </Drawer>
     );
   };
