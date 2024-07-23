593f1aa2493221571166cb411909efd7f222f874
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-07-23 14:12:18 +0100

    removing CORS hotfix

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,7 +5,7 @@ const coinAPIKey = "B1B01E56-174E-4475-A280-0B01BC9627F0";
 
 // CoinMarketCap API configuration
 const coinMarketCapAxiosInstance = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com",
+  baseURL: "https://pro-api.coinmarketcap.com",
 });
 
 coinMarketCapAxiosInstance.interceptors.request.use(
